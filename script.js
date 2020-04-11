efd7628fcbc5449bc77f2d6b17e277bc524dd00a
Drew Swinicki <dswinicki93@gmail.com>
2020-04-11 10:05:07 -0400

    updated script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var foodItemArray = [];
 var foodCaloriesArray = [];
 // Save user info to local storage 
@@ -23,22 +24,15 @@ function foodLogger() {
     })
         //
         .then(function (response) {
-            console.log(response);
-            console.log(response.hits[0].fields.item_name)
-            console.log(response.hits[0].fields.nf_calories)
-            console.log(response.hits[1].fields.item_name)
-            console.log(response.hits[1].fields.nf_calories)
-            console.log(response.hits[2].fields.item_name)
-            console.log(response.hits[2].fields.nf_calories)
-            console.log(response.hits[3].fields.item_name)
-            console.log(response.hits[3].fields.nf_calories)
-            console.log(response.hits[4].fields.item_name)
-            console.log(response.hits[4].fields.nf_calories)
-            console.log(response.hits[5].fields.item_name)
-            console.log(response.hits[5].fields.nf_calories)
-            console.log(response.hits[6].fields.item_name)
-            console.log(response.hits[6].fields.nf_calories);
+
+            var results = response.hits;
+            for (var i = 0; i < 7; i++) {
+                console.log(results[i].fields.item_name)
+                console.log(results[i].fields.nf_calories)
+            }
+
             $(".hide").removeClass("hide");
+
             $("#foodBtn0").text(response.hits[0].fields.item_name);
             $("#foodBtn1").text(response.hits[1].fields.item_name);
             $("#foodBtn2").text(response.hits[2].fields.item_name);
@@ -46,6 +40,7 @@ function foodLogger() {
             $("#foodBtn4").text(response.hits[4].fields.item_name);
             $("#foodBtn5").text(response.hits[5].fields.item_name);
             $("#foodBtn6").text(response.hits[6].fields.item_name);
+            
             $("#foodBtn0").attr("data-food-name", response.hits[0].fields.item_name);
             $("#foodBtn0").attr("data-food-calories", response.hits[0].fields.nf_calories)
             $("#foodBtn1").attr("data-food-name", response.hits[1].fields.item_name);
@@ -60,30 +55,30 @@ function foodLogger() {
             $("#foodBtn5").attr("data-food-calories", response.hits[5].fields.nf_calories)
             $("#foodBtn6").attr("data-food-name", response.hits[6].fields.item_name);
             $("#foodBtn6").attr("data-food-calories", response.hits[6].fields.nf_calories)
-            // }
+            
         });
-    };
-    $(".foodOption").click(saveFood);
-    // Save foods to local storage 
-    function saveFood() {
-        var foodItem = $(this).attr("data-food-name");
-        var foodCalories = $(this).attr("data-food-calories");
-        foodItemArray.push(foodItem);
-        foodCaloriesArray.push(foodCalories);
-        console.log(foodItem);
-        console.log(foodCalories);
-        $(".foodTableBody").empty();
-        console.log(foodItemArray);
-        for (i = 0; i < foodItemArray.length; i++) {
-            // Create a new table row element
-            var tRow = $("<tr>");
-            // Methods run on jQuery selectors return the selector they we run on
-            // This is why we can create and save a reference to a td in the same statement we update its text
-            var food = $("<td>").text(foodItemArray[i]);
-            var calories = $("<td>").text(foodCaloriesArray[i]);
-            // Append the newly created table data to the table row
-            tRow.append(food, calories);
-            // Append the table row to the table body
-            $("tbody").append(tRow);
-        }
-    };
+};
+$(".foodOption").click(saveFood);
+// Save foods to local storage 
+function saveFood() {
+    var foodItem = $(this).attr("data-food-name");
+    var foodCalories = $(this).attr("data-food-calories");
+    foodItemArray.push(foodItem);
+    foodCaloriesArray.push(foodCalories);
+    console.log(foodItem);
+    console.log(foodCalories);
+    $(".foodTableBody").empty();
+    console.log(foodItemArray);
+    for (i = 0; i < foodItemArray.length; i++) {
+        // Create a new table row element
+        var tRow = $("<tr>");
+        // Methods run on jQuery selectors return the selector they we run on
+        // This is why we can create and save a reference to a td in the same statement we update its text
+        var food = $("<td>").text(foodItemArray[i]);
+        var calories = $("<td>").text(foodCaloriesArray[i]);
+        // Append the newly created table data to the table row
+        tRow.append(food, calories);
+        // Append the table row to the table body
+        $("tbody").append(tRow);
+    }
+};
